1bec32cbf3eb33d1da5a93078584b41db84ce019
marvinRoellecke <115343305+marvinRoellecke@users.noreply.github.com>
2022-11-15 15:40:24 +0100

    add next and prev button (#4)
    * add next and prev button
    
    * show current and max page at navigation bar

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import { createCharacterCard } from "./components/card/card.js";
 
+console.clear();
+
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
@@ -11,22 +13,53 @@ const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
-const maxPage = 1;
+const dataCharacters = await fetchCharacters();
+const maxPage = dataCharacters.info.pages; //hier stand eine 1
 const page = 1;
 const searchQuery = "";
+let currentPage = 1;
 
-const dataCharacters = await fetchCharacters();
-dataCharacters.forEach((dataCharacter) => {
+dataCharacters.results.forEach((dataCharacter) => {
   cardContainer.append(createCharacterCard(dataCharacter));
 });
 
-//console.log(dataCharacters);
+pagination.textContent = `${currentPage} / ${maxPage}`;
 
-async function fetchCharacters() {
-  const response = await fetch("https://rickandmortyapi.com/api/character");
+async function fetchCharacters(pageIndex) {
+  const response = await fetch(
+    "https://rickandmortyapi.com/api/character?page=" + pageIndex
+  );
   const data = await response.json();
-  console.log(data.results);
-  return data.results;
+  console.log(data);
+  return data; //.results
 }
 
-//console.log(fetchCharacters());
+//Next und Previous Button
+
+nextButton.addEventListener("click", async () => {
+  if (currentPage >= maxPage) {
+    return;
+  } else {
+    cardContainer.innerHTML = "";
+    currentPage++;
+    const dataCharacters = await fetchCharacters(currentPage);
+    dataCharacters.results.forEach((dataCharacter) => {
+      cardContainer.append(createCharacterCard(dataCharacter));
+    });
+    pagination.textContent = `${currentPage} / ${maxPage}`;
+  }
+});
+
+prevButton.addEventListener("click", async () => {
+  if (currentPage <= 1) {
+    return;
+  } else {
+    cardContainer.innerHTML = "";
+    currentPage--;
+    const dataCharacters = await fetchCharacters(currentPage);
+    dataCharacters.results.forEach((dataCharacter) => {
+      cardContainer.append(createCharacterCard(dataCharacter));
+    });
+    pagination.textContent = `${currentPage} / ${maxPage}`;
+  }
+});

9fdb7c7034d2e6322d6510ee29c4226e862d65b3
Alexander Unmuth <alexander.unmuth@gmail.com>
2022-11-15 11:25:30 +0100

    add character infos

--- components/card/card.js
+++ components/card/card.js
@@ -25,22 +25,24 @@ export function createCharacterCard() {
   li.innerHTML = `  <div class="card__image-container">
   <img
     class="card__image"
-    src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
-    alt="Rick Sanchez"
+    src="${data.image}"
+    alt="${data.name}"
   />
   <div class="card__image-gradient"></div>
 </div>
 <div class="card__content">
-  <h2 class="card__title">Rick Sanchez</h2>
+  <h2 class="card__title">${data.name}</h2>
   <dl class="card__info">
     <dt class="card__info-title">Status</dt>
-    <dd class="card__info-description">Alive</dd>
+    <dd class="card__info-description">${data.status}</dd>
     <dt class="card__info-title">Type</dt>
-    <dd class="card__info-description"></dd>
+    <dd class="card__info-description">${data.type}</dd>
     <dt class="card__info-title">Occurrences</dt>
-    <dd class="card__info-description">51</dd>
+    <dd class="card__info-description">${data.episode.length}</dd>
   </dl>
 </div>`;
 
   return li;
 }
+
+const data = fetchCharacters();
